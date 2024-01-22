ddd73f1051e09c21814b3148000eef5f59df7915
Andrii Hutnyk <andreigutnik@gmail.com>
2024-01-22 07:56:49 +0100

    Add filter form

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,78 +1,190 @@
 // import { useDispatch } from 'react-redux';
-import { FilterWrap, Label } from './Filter.styled';
+import { useState } from 'react';
+import {
+  FilterWrap,
+  InputFrom,
+  InputTo,
+  InputWrap,
+  Label,
+  SearchButton,
+  SelectWrap,
+} from './Filter.styled';
 import Select from 'react-select';
 import { baseTheme } from 'theme';
 
 export const Filter = ({ brands }) => {
   // const dispatch = useDispatch();
+  const [selectedPriceStep, setSelectedPriceStep] = useState(null);
+  const [selectedPriceLabel, setSelectedPriceLabel] = useState('');
+  const [mileageFrom, setMileageFrom] = useState('');
+  const [mileageTo, setMileageTo] = useState('');
   const brandOptions = brands.map(brabd => ({
     value: brabd,
     label: brabd,
   }));
 
+  const priceOptions = [];
+
+  for (let i = 30; i <= 500; i += 10) {
+    priceOptions.push({ value: i, label: `${i}` });
+  }
+
+  const onChangePriceStep = selectedOption => {
+    setSelectedPriceStep(selectedOption.value);
+    setSelectedPriceLabel(selectedOption.label);
+  };
+
   return (
     <FilterWrap>
-      <Label htmlFor="selectBrand">Car brand</Label>
-      <Select
-        inputId="selectBrand"
-        placeholder="Enter the text"
-        isClearable={true}
-        // onChange={selectedOption => console.log(selectedOption)}
-        options={brandOptions}
-        styles={{
-          control: styles => ({
-            ...styles,
-            width: '224px',
-            border: 'none',
-            borderRadius: '14px',
-            margin: 0,
-            fontSize: '18px',
-            backgroundColor: `${baseTheme.colors.darkwhite}`,
-            appearance: 'none',
-            fontWeight: 500,
-            lineHeight: 'calc(20 / 18)',
-          }),
-          input: styles => ({
-            ...styles,
-            margin: 0,
-            paddingTop: '14px',
-            paddingBottom: '14px',
-            paddingLeft: '18px',
-            paddingRight: '18px',
-          }),
-          valueContainer: styles => ({
-            ...styles,
-            margin: 0,
-            padding: 0,
-          }),
-          placeholder: styles => ({
-            ...styles,
-            color: `${baseTheme.colors.black}`,
-            margin: 0,
-            paddingLeft: '18px',
-          }),
-          singleValue: styles => ({
-            ...styles,
-            margin: 0,
-            paddingLeft: '18px',
-          }),
-          menu: styles => ({
-            ...styles,
-            width: '224px',
-          }),
-          option: (styles, { isFocused }) => {
-            return {
+      <SelectWrap>
+        <Label htmlFor="selectBrand">Car brand</Label>
+        <Select
+          inputId="selectBrand"
+          placeholder="Enter the text"
+          isClearable={true}
+          // onChange={selectedOption => console.log(selectedOption)}
+          options={brandOptions}
+          styles={{
+            control: styles => ({
               ...styles,
-              color: isFocused
-                ? `${baseTheme.colors.black}`
-                : `${baseTheme.colors.grey}`,
-            };
-          },
-        }}
-        components={{
-          IndicatorSeparator: () => null,
-        }}
-      />
+              width: '224px',
+              border: 'none',
+              borderRadius: '14px',
+              margin: 0,
+              fontSize: '18px',
+              backgroundColor: `${baseTheme.colors.darkwhite}`,
+              appearance: 'none',
+              fontWeight: 500,
+              lineHeight: 'calc(20 / 18)',
+            }),
+            input: styles => ({
+              ...styles,
+              margin: 0,
+              paddingTop: '14px',
+              paddingBottom: '14px',
+              paddingLeft: '18px',
+              paddingRight: '18px',
+            }),
+            valueContainer: styles => ({
+              ...styles,
+              margin: 0,
+              padding: 0,
+            }),
+            placeholder: styles => ({
+              ...styles,
+              color: `${baseTheme.colors.black}`,
+              margin: 0,
+              paddingLeft: '18px',
+            }),
+            singleValue: styles => ({
+              ...styles,
+              margin: 0,
+              paddingLeft: '18px',
+            }),
+            menu: styles => ({
+              ...styles,
+              width: '224px',
+            }),
+            option: (styles, { isFocused }) => {
+              return {
+                ...styles,
+                color: isFocused
+                  ? `${baseTheme.colors.black}`
+                  : `${baseTheme.colors.grey}`,
+              };
+            },
+          }}
+          components={{
+            IndicatorSeparator: () => null,
+          }}
+        />
+      </SelectWrap>
+      <SelectWrap>
+        <Label htmlFor="selectPrice">Price/ 1 hour</Label>
+        <Select
+          inputId="selectPrice"
+          placeholder="To $"
+          value={
+            selectedPriceStep
+              ? { value: selectedPriceStep, label: selectedPriceLabel }
+              : null
+          }
+          onChange={onChangePriceStep}
+          options={priceOptions}
+          styles={{
+            control: styles => ({
+              ...styles,
+              width: '125px',
+              border: 'none',
+              borderRadius: '14px',
+              margin: 0,
+              fontSize: '18px',
+              backgroundColor: `${baseTheme.colors.darkwhite}`,
+              appearance: 'none',
+              fontWeight: 500,
+              lineHeight: 1.11,
+            }),
+            input: styles => ({
+              ...styles,
+              margin: 0,
+              paddingTop: '14px',
+              paddingBottom: '14px',
+              paddingLeft: '18px',
+              paddingRight: '18px',
+            }),
+            valueContainer: styles => ({
+              ...styles,
+              margin: 0,
+              padding: 0,
+            }),
+            placeholder: styles => ({
+              ...styles,
+              color: `${baseTheme.colors.black}`,
+              margin: 0,
+              paddingLeft: '18px',
+            }),
+            singleValue: styles => ({
+              ...styles,
+              margin: 0,
+              paddingLeft: '18px',
+            }),
+            menu: styles => ({
+              ...styles,
+              width: '224px',
+            }),
+            option: (styles, { isFocused }) => {
+              return {
+                ...styles,
+                color: isFocused
+                  ? `${baseTheme.colors.black}`
+                  : `${baseTheme.colors.grey}`,
+              };
+            },
+          }}
+          components={{
+            IndicatorSeparator: () => null,
+          }}
+        />
+      </SelectWrap>
+      <SelectWrap>
+        <Label htmlFor="inputMileage">Car mileage / km</Label>
+        <InputWrap>
+          <InputFrom
+            type="text"
+            id="inputMileage"
+            placeholder="From"
+            value={mileageFrom}
+            onChange={e => setMileageFrom(e.target.value)}
+          />
+          <InputTo
+            type="text"
+            placeholder="To"
+            value={mileageTo}
+            onChange={e => setMileageTo(e.target.value)}
+          />
+        </InputWrap>
+      </SelectWrap>
+      <SearchButton>Search</SearchButton>
     </FilterWrap>
   );
 };
--- src/components/Filter/Filter.styled.js
+++ src/components/Filter/Filter.styled.js
@@ -2,9 +2,19 @@ import styled from 'styled-components';
 import { baseTheme } from 'theme';
 
 export const FilterWrap = styled.div`
+  display: flex;
+  align-items: start;
+  justify-content: left;
+  flex-direction: column;
+  column-gap: 18px;
+  row-gap: 12px;
   padding-bottom: 24px;
 
   @media screen and (min-width: 768px) {
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    flex-direction: row;
     padding-bottom: 50px;
   }
 `;
@@ -16,4 +26,69 @@ export const Label = styled.label`
   font-weight: 500;
   line-height: calc(18 / 14);
   margin-bottom: 8px;
+`;
+
+export const SelectWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InputWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const inputStyles = `
+	display: flex;
+	width: 160px;
+	height: 48px;
+	padding: 14px 93px 14px 24px;
+	align-items: center;
+	flex-shrink: 0;  
+  border: none;
+  background-color: ${baseTheme.colors.darkwhite};
+  padding-left: 24px;  
+  
+	&:focus {
+    outline-color: rgba(138, 138, 137, 0.2);
+  }
+
+	&::placeholder{
+		color: ${baseTheme.colors.black};
+		font-size: 18px;
+		font-weight: 500;
+		line-height: calc(20 / 18);
+	}
+`;
+
+export const InputFrom = styled.input`
+  ${inputStyles}
+  border-radius: 14px 0px 0px 14px;
+  border-right: 1px solid rgba(138, 138, 137, 0.2);
+`;
+
+export const InputTo = styled.input`
+  ${inputStyles}
+  border-radius: 0px 14px 14px 0px;
+`;
+
+export const SearchButton = styled.button`
+  width: 136px;
+  padding: 14px;
+  margin-top: auto;
+  margin-bottom: 0;
+  color: ${baseTheme.colors.white};
+  font-size: 14px;
+  font-weight: 600;
+  line-height: calc(20 / 14);
+  border: transparent;
+  border-radius: 12px;
+  background-color: ${baseTheme.colors.blue};
+  cursor: pointer;
+  transition: background-color 250ms ${baseTheme.animation.cubicBezier};
+
+  &:hover,
+  &:focus {
+    background-color: ${baseTheme.colors.darkblue};
+  }
 `;
